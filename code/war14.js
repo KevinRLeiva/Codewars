0f381d4ad35c9928be82a80a478ea9adbc2084e6
Kevin R Leiva <47392702+KevinRLeiva@users.noreply.github.com>
2019-03-27 16:09:35 -0400

    an array function

---
+++ code/war14.js
@@ -0,0 +1,8 @@
+//given an array of integers, return the numbers of entries which are greater than 10
+function countTo10(integers){
+  let count = 0;
+  for(let i = 0; i < integers.length; i++){
+    count++
+  }
+  return count;
+}
